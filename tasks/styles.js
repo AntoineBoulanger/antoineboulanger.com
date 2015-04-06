dc1bc3799ab964c11eaa272e01e66a0b441119b6
Antoine Boulanger <hello@antoineboulanger.com>
2015-04-06 17:36:31 -0400

    Initial gulp

---
+++ tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+gulp.task('styles', function () {
+	return gulp.src('src/styles/main.less')
+		.pipe(less({
+			strictMath: true
+		}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('.tmp/styles'))
+		.pipe(browserSync.reload({stream: true}));
+});
