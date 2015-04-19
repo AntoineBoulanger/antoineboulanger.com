571695eaf122db8610e969290ca762cad841eb70
Antoine Boulanger <hello@antoineboulanger.com>
2015-04-19 11:27:27 -0400

    Add deploy extras

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -2,12 +2,7 @@ var gulp = require('gulp');
 var size = require('gulp-size');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
-
-gulp.task('deploy', ['html', 'css', 'images'], function () {
+gulp.task('deploy', ['html', 'css', 'images', 'extras'], function () {
 	return gulp.src('dist/**/*')
 		.pipe(size({title: 'build', gzip: true}))
 		.pipe(ghPages());
