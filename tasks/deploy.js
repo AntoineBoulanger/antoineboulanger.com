a6497a4044edd7c75c092d8ca1035e6196318bf3
Antoine Boulanger <hello@antoineboulanger.com>
2015-04-19 10:52:06 -0400

    Deploy to gh-pages

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
 var size = require('gulp-size');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task('deploy', ['html', 'css', 'images'], function () {
-	return gulp.src('dist/**/*').pipe(size({title: 'build', gzip: true}));
+	return gulp.src('dist/**/*')
+		.pipe(size({title: 'build', gzip: true}))
+		.pipe(ghPages());
 });
